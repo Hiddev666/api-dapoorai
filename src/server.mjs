880cd8b09e785ad2a34afefe9ba0324e2e66ba76
Hiddev666 <officehiddev@gmail.com>
2025-05-09 09:42:05 +0700

    dev: add router skeleton

--- src/server.mjs
+++ src/server.mjs
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import db from "./lib/db.mjs"
+import router from "./routes/router.mjs"
 
 dotenv.config()
 
@@ -9,11 +11,40 @@ const PORT = process.env.SERVER_PORT
 
 // Middelwares
 app.use(express.json())
+app.use(router)
 
 app.get("/", (req, res) => {
     res.send({
         message: "dapoorAI REST API | Build with love by Hiddev"
     })
+})
+
+app.get("/test", async (req, res) => {
+    try {
+        const test = await db.test.findMany()
+        res.status(200).send({
+            message: "Successfully Get Test",
+            data: test
+        })
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+})
+
+
+app.post("/test", async (req, res) => {
+    try {
+        const { body } = req
+        const test = await db.test.create({
+            data: body
+        })
+        res.status(200).send({
+            message: "Successfully POST Test",
+            data: test
+        })
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
 })
 
 app.listen(PORT, () => {
